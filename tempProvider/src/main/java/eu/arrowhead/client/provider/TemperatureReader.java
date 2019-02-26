56398e2f3b5cde62b5230aeefb60b297e0ccbfb1
samuelandersson97 <sammeandersson97@gmail.com>
2019-02-26 11:08:54 +0100

    Automatical modprobe execution

--- tempProvider/src/main/java/eu/arrowhead/client/provider/TemperatureReader.java
+++ tempProvider/src/main/java/eu/arrowhead/client/provider/TemperatureReader.java
@@ -3,11 +3,19 @@ package eu.arrowhead.client.provider;
 import com.pi4j.component.temperature.TemperatureSensor;
 import com.pi4j.io.w1.W1Master;
 import com.pi4j.temperature.TemperatureScale;
+import java.io.*;
 
 
 public class TemperatureReader{
   
   public double readTemperature(){
+      
+      try{
+        Runtime.getRuntime().exec("sudo modprobe w1-gpio");
+        Runtime.getRuntime().exec("sudo modprobe w1-therm");
+      }catch(Exception e){
+        System.out.println("You need to run: 'sudo modprobe w1-gpio' AND 'sudo modprobe w1-therm' manually before running startUp.sh");
+      }
    
       W1Master w1Master = new W1Master();
     
